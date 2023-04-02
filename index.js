5ecff93db2356862aad0c5a3d39a1e3a3db46be5
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-04-01 20:44:26 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(drivers, attribute)
+  {
+     
+        return drivers.filter(function(driver) {
+            if (driver.toLowerCase() === attribute.toLowerCase())
+            {
+                return driver;
+            }
+        })
+
+
+  }
+  function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+  }
+  
+  function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
